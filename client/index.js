4a4b41335759a6b442fcabbf05b06e8206644cba
Jon Edwards <jkedwards@me.com>
2015-04-14 16:55:42 -0700

    add fight routine based on character attributes

--- client/index.js
+++ client/index.js
@@ -7,7 +7,67 @@ $(document).ready(init);
 
 function init(){
   drawPokedex();
-  $('#pokedex').on('click', '.pokemon', getPokemon)
+  $('#pokedex').on('click', '.pokemon:not(.filled)', getPokemon);
+  $('#pokedex').on('click', '.filled', pickPlayers)
+  $('#clear').click(clearPokemon);
+  $('#fight').click(fight);
+}
+
+function pickPlayers(){
+  if($(this).hasClass('p1')){
+    $(this).removeClass('p1').addClass('p2');
+  }
+  else if($(this).hasClass('p2')){
+    $(this).removeClass('p2');
+  }
+  else{
+    $(this).addClass('p1');
+  }
+}
+
+function fight(){
+  var $p1 = $('.p1');
+  var $p2 = $('.p2');
+
+  var p1 = {}, p2 = {};
+
+  p1.attack = $('.p1').find('li:nth-child(1)').text().split(':')[1] * 1;
+  p1.defense = $('.p1').find('li:nth-child(2)').text().split(':')[1] * 1;
+  p1.exp = $('.p1').find('li:nth-child(3)').text().split(':')[1] * 1;
+  p1.hp = $('.p1').find('li:nth-child(4)').text().split(':')[1] * 1;
+  p2.attack = $('.p2').find('li:nth-child(1)').text().split(':')[1] * 1;
+  p2.defense = $('.p2').find('li:nth-child(2)').text().split(':')[1] * 1;
+  p2.exp = $('.p2').find('li:nth-child(3)').text().split(':')[1] * 1;
+  p2.hp = $('.p2').find('li:nth-child(4)').text().split(':')[1] * 1;
+
+  hit(p1, p2);
+  hit(p2, p1);
+
+  $('.p1').find('li:nth-child(4)').text('hp:' + p1.hp);
+  $('.p2').find('li:nth-child(4)').text('hp:' + p2.hp);
+
+  if(p1.hp <= 0){
+    $p1.remove();
+  }
+  if(p2.hp <= 0){
+    $p2.remove();
+  }
+}
+
+function hit(p1, p2){
+  var attack = p1.attack * (p1.exp / 100);
+  var defense = p2.defense * (p2.exp / 100);
+  var final = attack - defense;
+  final = 0 || final;
+  var random = Math.floor(Math.random() * final);
+  console.log(random);
+
+  $('.p2').find('li:nth-child(4)').text()
+  p2.hp -= random;
+}
+
+function clearPokemon(){
+  $('.pokemon:not(.filled)').remove();
 }
 
 function drawPokedex(){
@@ -34,23 +94,22 @@ function getPokemon(){
   var uri = $(this).data('uri');
   var url = domain + uri;
   $.getJSON(url, function(response){
-    console.log(response);
+    $self.addClass('filled');
 
     var $ul = $self.find('ul');
     var attributes = ['attack', 'defense', 'exp', 'hp'];
 
     var $lis = attributes.map(function(attribute){
-      return '<li>' + attribute + ':' + response[attribute] + '</li>';
+      return '<li>' + attribute + ': ' + response[attribute] + '</li>';
     });
 
-    $ul.append($lis);
+    $ul.empty().append($lis);
 
     var spriteUrls = response.sprites.map(function(obj){
       return domain + obj.resource_uri;
     });
     spriteUrls.forEach(function(url){
       $.getJSON(url, function(response){
-        console.log(domain + response.image);
         $self.children('.image').css('background-image', 'url(' + domain + response.image + ')');
       });
     });
