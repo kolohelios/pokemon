19c3ffb5e136d3aed02b5bedb851c517daa16a3d
Jon Edwards <jkedwards@me.com>
2015-04-14 15:50:04 -0700

    adding attributes for pokemons

--- client/index.js
+++ client/index.js
@@ -13,13 +13,18 @@ function init(){
 function drawPokedex(){
   pokedex.pokemon.forEach(function(pokemonObj){
     var $outerDiv = $('<div>');
-    $outerDiv.addClass('pokemon').attr('data-uri', pokemonObj.resource_uri);
     var $name = $('<div>');
-    $name.addClass('name').text(pokemonObj.name);
     var $image = $('<div>');
+    var $stats = $('<div>');
+    $outerDiv.addClass('pokemon').attr('data-uri', pokemonObj.resource_uri);
+    $name.addClass('name').text(pokemonObj.name);
     $image.addClass('image');
+    $stats.addClass('stats');
 
-    $('#pokedex').append($outerDiv.append($name, $image));
+    var $ul = $('<ul>');
+    $stats.append($ul);
+
+    $('#pokedex').append($outerDiv.append($name, $image, $stats));
   });
 }
 
@@ -29,6 +34,17 @@ function getPokemon(){
   var uri = $(this).data('uri');
   var url = domain + uri;
   $.getJSON(url, function(response){
+    console.log(response);
+
+    var $ul = $self.find('ul');
+    var attributes = ['attack', 'defense', 'exp', 'hp'];
+
+    var $lis = attributes.map(function(attribute){
+      return '<li>' + attribute + ':' + response[attribute] + '</li>';
+    });
+
+    $ul.append($lis);
+
     var spriteUrls = response.sprites.map(function(obj){
       return domain + obj.resource_uri;
     });
