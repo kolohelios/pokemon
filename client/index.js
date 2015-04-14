0b0867305fb98424b41ae11a34d88e425e8eb971
Jon Edwards <jkedwards@me.com>
2015-04-14 10:40:47 -0700

    add cache of pokedex and basic display of pokedex

--- client/index.js
+++ client/index.js
@@ -1,1 +1,17 @@
 'use strict';
+
+$(document).ready(init);
+
+function init(){
+  drawPokedex();
+}
+
+function drawPokedex(){
+  pokedex.pokemon.forEach(function(pokemonObj){
+    var $outerDiv = $('<div>');
+    $outerDiv.addClass('pokemon').attr('data-uri', pokemonObj.resource_uri);
+    var $innerDiv = $('<div>');
+    $innerDiv.addClass('name').text(pokemonObj.name);
+    $('#pokedex').append($outerDiv.append($innerDiv));
+  });
+}
